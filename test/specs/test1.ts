9f0a4159c719713b415d00528f57f6632d098311
DmytroVornik <74198155+DmytroVornik@users.noreply.github.com>
2020-11-12 13:48:28 +0200

    asas

--- test/specs/test1.ts
+++ test/specs/test1.ts
@@ -1,10 +1,9 @@
-/*describe('adding and removing', () => {
+describe('adding and removing', () => {
     it('added 10 button delete', () => {
         browser.url(`https://the-internet.herokuapp.com/add_remove_elements/`);
 
-        let count: number = 0;
+        
         for(let i = 0;i < 10;i++)
-        count+=i;
         $('button').click(); 
 
 
@@ -12,4 +11,4 @@
 
 
       });
-});*/
+});
