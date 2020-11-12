cbe5db1981b9f0b4b8fabcb5dcd6bbe85071817e
DmytroVornik <74198155+DmytroVornik@users.noreply.github.com>
2020-11-12 13:34:02 +0200

    1

--- built/basic.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-describe('webdriver.io page', () => {
-    it('should have the right title', () => {
-        browser.url('https://google.com');
-        expect(browser).toHaveTitle('Google');
-    });
-});
