307333298cfd96566bbb8bd3c759063cdb822c10
DmytroVornik <74198155+DmytroVornik@users.noreply.github.com>
2020-11-12 23:38:55 +0200

    A

--- test/specs/test1.ts
+++ test/specs/test1.ts
@@ -1,9 +1,6 @@
 describe('adding and removing', () => {
     it('added 10 button delete', () => {
         browser.url(`https://the-internet.herokuapp.com/add_remove_elements/`);
-
-        
-        for(let i = 0;i < 10;i++)
         $('button').click(); 
 
 
--- test/specs/test2.ts
+++
@@ -1,9 +0,0 @@
-describe('adding and removing', () => {
-    it('added 10 button delete', () => {
-        browser.url(`https://the-internet.herokuapp.com/add_remove_elements/`);
-        $('button').click();     
-        
-        expect($('.added-manually').isExisting);
-
-    });
-});
