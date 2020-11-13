1c999098dd024ff6ac480483100c575a954bb079
DmytroVornik <74198155+DmytroVornik@users.noreply.github.com>
2020-11-13 10:56:58 +0200

    1

--- test/specs/test1.ts
+++ test/specs/test1.ts
@@ -1,11 +1,86 @@
 describe('adding and removing', () => {
-    it('added 10 button delete', () => {
+    beforeAll((done) => {
         browser.url(`https://the-internet.herokuapp.com/add_remove_elements/`);
-        $('button').click(); 
+        //expect($('.button').isClickable);
+        //expect($('.button').isExisting);
+    });
+
+    it('adding first button delete', () => {
+        $('button').click();
+        expect($$('.added-manually')[0].isExisting);
+
+    });
+
+    it('adding second button delete', () => {
+        expect($$('.added-manually')[0].isExisting);
+
+        $('button').click();
+
+        expect($$('.added-manually')[1].isExisting);
+        expect($$('.added-manually').length).toEqual(2);
+    });
 
 
-        expect($('.added-manually').isExisting);
+    it('adding third button delete', () => {
+        expect($$('.added-manually').length).toEqual(2);
+
+        $('button').click();
+
+        expect($$('.added-manually')[2].isExisting);
+        expect($$('.added-manually').length).toEqual(3);
+    });
 
 
-      });
+    it('adding fourth and fifth button delete', () => {
+        expect($$('.added-manually').length).toEqual(3);
+
+        $('button').click();
+        $('button').click();
+
+        expect($$('.added-manually').length).toEqual(5);
+
+    });
+
+    it(('deleting middle button delete'), () => {
+        expect($$('.added-manually')[2].isExisting);
+        expect($$('.added-manually').length).toEqual(5);
+
+        $$('.added-manually')[2].click();
+
+        expect($$('.added-manually').length).toEqual(4);
+
+    });
+
+
+    it(('deleting first button delete'), () => {
+        expect($$('.added-manually')[0].isExisting);
+        expect($$('.added-manually').length).toEqual(4);
+
+        $$('.added-manually')[0].click();
+
+        expect($$('.added-manually').length).toEqual(3);
+
+    });
+
+    it(('deleting last button delete'), () => {
+        expect($$('.added-manually')[2].isExisting);
+        expect($$('.added-manually').length).toEqual(3);
+
+        $$('.added-manually')[2].click();
+
+        expect($$('.added-manually').length).toEqual(2);
+
+    });
+
+    it(('deleting all buttons delete'), () => {
+        expect($$('.added-manually').length).toEqual(2);
+
+        $$('.added-manually')[1].click();
+        $$('.added-manually')[0].click();
+
+        expect($$('.added-manually').length).toEqual(0);
+
+    });
+
+
 });
