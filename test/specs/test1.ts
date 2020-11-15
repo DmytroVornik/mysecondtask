6d6d86d9dc1e8c581ac86a928824a69daaf3ac17
DmytroVornik <74198155+DmytroVornik@users.noreply.github.com>
2020-11-15 16:02:57 +0200

    1

--- test/specs/test1.ts
+++ test/specs/test1.ts
@@ -5,18 +5,21 @@ describe('adding and removing', () => {
     beforeAll(() => {
         browser.url(`https://the-internet.herokuapp.com/add_remove_elements/`);
     });
-
+    
+    //initial state for each test
     beforeEach(() => {
         expect($(addButton)).toBeClickable();
     });
 
+    //Check that when you click on the Add button, one Delete button is created
     it('adding first button delete', () => {
         
         $(addButton).click();
         expect($$(deleteButton)).toBeElementsArrayOfSize(1);
 
     });
-
+    //Check that when you click on the Add button, a second Delete button is created. 
+    //Now there are two
     it('adding second button delete', () => {
 
         $(addButton).click();
@@ -25,7 +28,8 @@ describe('adding and removing', () => {
 
     });
 
-
+    //Check that when you click on the Add button, a third Delete button is created.
+    //Now there are three
     it('adding third button delete', () => {
 
         $(addButton).click();
@@ -33,7 +37,8 @@ describe('adding and removing', () => {
 
     });
 
-
+    //Check that by two clicks on the Add button, the fourth and fifth Delete buttons are created.
+    // Now there are five
     it('adding fourth and fifth button delete', () => {
 
         $(addButton).click();
@@ -43,6 +48,8 @@ describe('adding and removing', () => {
 
     });
 
+    //Make sure that when you click on the Delete button, which is in the middle, 
+    //the indicated button is deleted and there are four items
     it(('deleting middle button delete'), () => {
 
         $$(deleteButton)[2].click();
@@ -52,6 +59,8 @@ describe('adding and removing', () => {
     });
 
 
+    //Make sure that when you click on the Delete button, which is at the beginning of the list, 
+    //the specified button is removed and three items remain.
     it(('deleting first button delete'), () => {
 
         $$(deleteButton)[0].click();
@@ -60,6 +69,9 @@ describe('adding and removing', () => {
 
     });
 
+
+    //Make sure that when you click on the "Delete" button at the end of the list, 
+    //the specified button is removed and two items remain.
     it(('deleting last button delete'), () => {
 
         $$(deleteButton)[2].click();
@@ -68,6 +80,8 @@ describe('adding and removing', () => {
 
     });
 
+
+    //Make sure that two clicks on the Delete button remove the remaining two items
     it(('deleting all buttons delete'), () => {
 
         $$(deleteButton)[1].click();
